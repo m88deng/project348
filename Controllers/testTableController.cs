76ed6a98e50aab7cd8f43b5b4fef70388bdd057f
Percival Li <p5li@uwaterloo.ca>
2024-07-09 14:50:50 -0400

    111

--- Controllers/testTableController.cs
+++ Controllers/testTableController.cs
@@ -8,65 +8,99 @@ namespace testAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class testTableController : ControllerBase
+    public class cs348Controller : ControllerBase
     {
-        [HttpPost]
-        public IActionResult Post(string sentence)
+        protected SqlConnection GetConnectionString()
         {
-            try
-            {
-                using var conn = new SqlConnection(
+                var conn = new SqlConnection(
                 new SqlConnectionStringBuilder()
                 {
                     DataSource = "MIKU39",
-                    InitialCatalog = "testDB",
+                    InitialCatalog = "cs348",
                     UserID = "root",
                     Password = "123456",
                     Encrypt = true,
                     TrustServerCertificate = true
                 }.ConnectionString
                 );
-                var sql = "INSERT INTO testTable(sentence) VALUES (@sentence);";
-                var command = new SqlCommand(sql, conn);
-                command.Parameters.Add("@sentence", SqlDbType.VarChar);
-                command.Parameters["@sentence"].Value = sentence;
-
-                conn.Open();
-                var row = command.ExecuteNonQuery();
-                if (row == 1)
-                {
-                    return StatusCode(201);
-                }
-                else
-                {
-                    return UnprocessableEntity();
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
+            return conn;
         }
+
+
+        //[HttpPost]
+        //public IActionResult Post(string sentence)
+        //{
+        //    try
+        //    {
+        //        var sql = "INSERT INTO testTable(sentence) VALUES (@sentence);";
+        //        var conn = GetConnectionString();
+        //        var command = new SqlCommand(sql, conn);
+        //        command.Parameters.Add("@sentence", SqlDbType.VarChar);
+        //        command.Parameters["@sentence"].Value = sentence;
+
+        //        conn.Open();
+        //        var row = command.ExecuteNonQuery();
+        //        if (row == 1)
+        //        {
+        //            return StatusCode(201);
+        //        }
+        //        else
+        //        {
+        //            return UnprocessableEntity();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(500, ex.Message);
+        //    }
+        //}
 
         [HttpGet("{query}")]
         public IActionResult GetByQuery(string query)
         {
-            using var conn = new SqlConnection(
-                new SqlConnectionStringBuilder()
-                {
-                    DataSource = "MIKU39",
-                    InitialCatalog = "testDB",
-                    UserID = "root",
-                    Password = "123456",
-                    Encrypt = true,
-                    TrustServerCertificate = true
-                }.ConnectionString
-                );
-            var command = new SqlCommand(query, conn);
+            using var conn = GetConnectionString();
 
             conn.Open();
             DataTable dataTable = new DataTable();
             SqlDataAdapter dataAdapter = new SqlDataAdapter(query, conn);
+            dataAdapter.Fill(dataTable);
+
+            return Ok(ConvertDataTableToJson(dataTable));
+        }
+
+        [HttpGet("{ID:int}")]
+        public IActionResult GetByID(int ID)
+        {
+            using var conn = GetConnectionString();
+
+            //              SELECT trip_id, departure_time
+            //              FROM Trip_Table
+            //              WHERE stop_id = 1001;
+            var command = "SELECT trip_id, departure_time FROM StopTimes WHERE stop_id = " + ID + ";";
+
+            conn.Open();
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command, conn);
+            dataAdapter.Fill(dataTable);
+
+            return Ok(ConvertDataTableToJson(dataTable));
+        }
+
+
+        [HttpGet]
+        [Route("api/info/[controller]")]
+        public IActionResult GetBusOperatesOnWeekend()
+        {
+            using var conn = GetConnectionString();
+
+            //              SELECT service_id, start_date, end_date
+            //              FROM Calendar_Table
+            //              WHERE saturday = 1 AND sunday = 1;
+            var command = "SELECT service_id FROM CalendarDates WHERE service_id LIKE 'saturday' AND service_id LIKE 'sunday'; ";
+
+            conn.Open();
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command, conn);
             dataAdapter.Fill(dataTable);
 
             return Ok(ConvertDataTableToJson(dataTable));
