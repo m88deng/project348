8326918388d6d6f2db01f93fe9e34db4bcb82583
m88deng <melideng123@gmail.com>
2024-07-21 09:48:35 -0400

    api connection test

--- client/src/pages/LookupSchedule.js
+++ client/src/pages/LookupSchedule.js
@@ -3,22 +3,30 @@ import { StyledLookupSchedule } from "../styles/LookupSchedule.styled";
 
 export default function LookupSchedule() {
     const [route, setRoute] = useState('');
-    const [Direction, setDirection] = useState('');
-    const [wheelchair, setWheelchair] = useState(0);
+    const [direction, setDirection] = useState('');
+    const [wheelchair, setWheelchair] = useState(false);
+
+    const url = `http://localhost:5290/3`;
 
     const handleScheduleSearch = async (e) => {
         e.preventDefault();
-        const res = await fetch("http://localhost:5290/3",{method:"GET"})
+        console.log("Route " + route);
+        console.log("direction " + direction);
+        console.log("wheelchair " + wheelchair);
+        const res = await fetch(url, { method: "GET" })
         console.log(res.json());
     }
     return (
         <StyledLookupSchedule className="container">
-            <form>
+            <form id="scheduleForm">
                 <div><label>Route</label></div>
                 <div><input type="text" value={route} onChange={(e) => setRoute(e.target.value)} required /></div>
                 <div><label>Direction</label></div>
-                <div><input type="text" value={Direction} onChange={(e) => setDirection(e.target.value)} /></div>
-                <div><input type="checkbox" /><label>Wheelchair boarding available</label></div>
+                <div><input type="text" value={direction} onChange={(e) => setDirection(e.target.value)} /></div>
+                <div>
+                    <input type="checkbox" checked={wheelchair} onChange={(e) => setWheelchair(e.target.checked)} />
+                    <label>Wheelchair boarding available</label>
+                </div>
                 <div><button type="submit" onClick={handleScheduleSearch}>Search</button></div>
             </form>
         </StyledLookupSchedule>
