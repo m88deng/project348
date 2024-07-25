9d73cd179f08b8f3c9105149f48c5c3ceb0e8884
m88deng <melideng123@gmail.com>
2024-07-25 15:33:12 -0400

    login/signup

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
 import './header.css';
 
-export default function Header() {
+export default function Header({ canLogin }) {
     const [activeLink, setActiveLink] = useState('');
     const location = useLocation();
+    const { auth } = useAuth();
+
+
 
     const handleLinkClick = (link) => {
         setActiveLink(link);
@@ -42,15 +46,36 @@ export default function Header() {
                             Upcoming Transit
                         </Link>
                     </div>
-                    <div className="col">
-                        <Link
-                            to="/my-account"
-                            className={location.pathname === '/my-account' ? 'active' : ''}
-                            onClick={() => handleLinkClick('/my-account')}
-                        >
-                            My Account
-                        </Link>
-                    </div>
+                    {auth.canLogin ? (
+                        <div className="col">
+                            {auth.login ? (
+                                <Link
+                                    to="/login"
+                                    className={location.pathname === '/login' ? 'active' : ''}
+                                    onClick={() => handleLinkClick('/login')}
+                                >
+                                    Login
+                                </Link>
+                            ) : (
+                                <Link
+                                    to="/signup"
+                                    className={location.pathname === '/signup' ? 'active' : ''}
+                                    onClick={() => handleLinkClick('/signup')}
+                                >
+                                    Signup
+                                </Link>
+                            )}
+                        </div>
+                    ) :
+                        (<div className="col">
+                            <Link
+                                to="/my-account"
+                                className={location.pathname === '/my-account' ? 'active' : ''}
+                                onClick={() => handleLinkClick('/my-account')}
+                            >
+                                My Account
+                            </Link>
+                        </div>)}
                 </div>
             </nav>
         </div>
---
+++ client/src/pages/Login.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './../styles/Login.css';
+import { useAuth } from '../contexts/AuthContext';
+
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [pwd, setPwd] = useState('');
+    const { setAuth } = useAuth();
+    const navigate = useNavigate();
+
+    const handleSignup = () => {
+        setAuth({canLogin: true, userId: null, email: null, login: false});
+        navigate('/signup');
+    };
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        var url = `http://localhost:5290/login/${email}/${pwd}`;
+
+        try {
+            const res = await fetch(url, { method: "GET" });
+
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            const resText = await res.text();
+            if (resText) {
+                const data = JSON.parse(resText);
+                console.log("Fetched data:", data);
+                console.log("userId: "+data[0].user_id);
+                setAuth({ canLogin: false, userId: data[0].user_id, email, login: true});
+                navigate('/my-account');
+                
+            } else {
+                console.log("Incorrect email / password.");
+            }
+        } catch (error) {
+            console.error("Error finding user", error);
+        }
+    }
+
+    return (
+        <div className='container'>
+            <form>
+                <div><label>Email</label></div>
+                <div><input type='text' value={email} onChange={(e) => (setEmail(e.target.value))} /></div>
+                <div><label>Password</label></div>
+                <div><input type='password' value={pwd} onChange={(e) => (setPwd(e.target.value))} /></div>
+                <div className='py-3'><p onClick={handleSignup}>Don't have an account? Sign up here!</p></div>
+                <div><button type='submit' onClick={handleLogin}>Login</button></div>
+            </form>
+        </div>
+    );
+}
